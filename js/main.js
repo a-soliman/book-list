179708154e4ee311f748f135e25eab6e186bf7b4
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-06-14 15:20:08 -0700

    coded the UI class

--- js/main.js
+++ js/main.js
@@ -6,3 +6,68 @@ class Book {
         this.isbn = isbn;
     }
 }
+
+// UI CONSTRUCTOR
+class UI {
+    constructor() {}
+
+    displayBooks() {
+        const books = lStorage.getBooks();
+        books.forEach( ( book ) => {
+            this.addBookToList(book);
+        });
+    }
+
+    addBookToList( book ) {
+        const bookList  = document.querySelector('#book-list');
+        const newRow    = document.createElement('tr');
+    
+        newRow.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td>
+                <span class='delete text-danger'>&times;</span>
+            </td>
+        `;
+        bookList.appendChild(newRow);
+    }
+
+    clearFields() {
+        document.querySelector('#title').value  = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value   = '';
+    }
+
+    removeBook(row) {
+        row.remove();
+    }
+
+    showAlert(type, message) {
+        const container = document.querySelector('.col');
+        const form      = document.querySelector('form');
+        const alertMSG  = document.createElement('div');
+    
+        alertMSG.className = 'alert';
+        if ( !type ) {
+            alertMSG.classList.add('alert-danger');
+        } else {
+            alertMSG.classList.add('alert-success');
+        }
+        alertMSG.innerText = message;
+        
+        container.insertBefore(alertMSG, form);
+        return;
+    }
+
+    removeAlert() {
+        document.querySelector('.alert').remove();
+        return;
+    }
+
+    scrollToTop() {
+        window.scrollTo(0, 0);
+        return;
+    }
+
+}
